a995f91b1aeeb66b1eb58b21c67545f4ddcef456
Abdellateef Eid <abdellateef35@gmail.com>
2025-05-03 18:53:55 +0300

    Add Book CRUD Ops

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,16 @@ import { CheckoutComponent } from './features/checkout/checkout.component';
 import { OrderConfirmationComponent } from './features/order-confirmation/order-confirmation.component';
 import { OrderHistoryComponent } from './features/order-history/order-history.component';
 import { OrderDetailsComponent } from './features/order-details/order-details.component';
+import { BookFormComponent } from './features/books/book-form/book-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'books', component: BookListComponent },
+
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/edit/:id', component: BookFormComponent },
   { path: 'books/:id', component: BookDetailsComponent },
+
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'order-confirmation/:id', component: OrderConfirmationComponent },
